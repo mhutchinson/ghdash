b641f4a248c3727324520d9b8fa02af9c8aea79b
Martin Hutchinson <mhutchinson@gmail.com>
2022-05-13 17:06:07 +0100

    Count the number of issues and PRs

--- main.go
+++ main.go
@@ -3,21 +3,26 @@ package main
 import (
 	"context"
 	"flag"
-	"github.com/google/go-github/v44/github"
+
 	"github.com/golang/glog"
+	"github.com/google/go-github/v44/github"
 )
 
 func main() {
 	flag.Parse()
-	glog.Info("hello")
+	ctx := context.Background()
 
 	client := github.NewClient(nil)
 
-	// list all organizations for user "willnorris"
-	orgs, _, err := client.Organizations.List(context.Background(), "willnorris", nil)
+	issues, _, err := client.Issues.ListByRepo(ctx, "google", "trillian", nil)
 	if err != nil {
 		glog.Exitf("Boom: %v", err)
 	}
-	glog.Infof("Found %d orgs", len(orgs))
-}
+	glog.Infof("Found %d issues", len(issues))
 
+	prs, _, err := client.PullRequests.List(ctx, "google", "trillian", nil)
+	if err != nil {
+		glog.Exitf("Boom: %v", err)
+	}
+	glog.Infof("Found %d PRs", len(prs))
+}
