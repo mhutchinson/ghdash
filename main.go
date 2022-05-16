794750705f8e2867673c50626580a510a60f75f2
Martin Hutchinson <mhutchinson@gmail.com>
2022-05-16 11:32:55 +0100

    Print out all the issues & PRs
    Also allow the repo to be selected via a flag

--- main.go
+++ main.go
@@ -3,26 +3,65 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
+	"strings"
+	"time"
 
 	"github.com/golang/glog"
 	"github.com/google/go-github/v44/github"
+)
+
+var (
+	repo = flag.String("repo", "google/trillian", "'owner/project' components of the github repo to get stats for")
 )
 
 func main() {
 	flag.Parse()
 	ctx := context.Background()
 
+	owner, project := parseRepo()
 	client := github.NewClient(nil)
 
-	issues, _, err := client.Issues.ListByRepo(ctx, "google", "trillian", nil)
+	issues, _, err := client.Issues.ListByRepo(ctx, owner, project, nil)
 	if err != nil {
 		glog.Exitf("Boom: %v", err)
 	}
 	glog.Infof("Found %d issues", len(issues))
+	for _, issue := range issues {
+		glog.Infof(" %s", formatItem(workItem(issue)))
+	}
 
-	prs, _, err := client.PullRequests.List(ctx, "google", "trillian", nil)
+	prs, _, err := client.PullRequests.List(ctx, owner, project, nil)
 	if err != nil {
 		glog.Exitf("Boom: %v", err)
 	}
 	glog.Infof("Found %d PRs", len(prs))
+	for _, pr := range prs {
+		glog.Infof(" %s", formatItem(workItem(pr)))
+	}
+}
+
+func parseRepo() (string, string) {
+	ss := strings.Split(*repo, "/")
+	if l := len(ss); l > 2 {
+		glog.Exitf("Expected owner/project, but got %d components", l)
+	}
+	return ss[0], ss[1]
+}
+
+type workItem interface {
+	GetNumber() int
+	GetTitle() string
+	GetUser() *github.User
+	GetAssignee() *github.User
+	GetCreatedAt() time.Time
+	GetUpdatedAt() time.Time
+}
+
+func formatItem(i workItem) string {
+	assignee := "UNASSIGNED"
+	if ass := i.GetAssignee(); ass != nil {
+		assignee = *ass.Login
+	}
+	return fmt.Sprintf("#%d %q: %s -> %s (%s, %s)", i.GetNumber(), i.GetTitle(), *i.GetUser().Login, assignee, i.GetCreatedAt(), i.GetUpdatedAt())
 }
